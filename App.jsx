70251a785cc6abe5b8cdd95f9c70c7e4fffab9e8
Gopal Sahu <gopalsahu25122004@gmail.com>
2026-02-17 11:24:39 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Tracker from "./components/Tracker";
+import Tracker from "./Tracker";
 import GlobalStyles from "./GlobalStyle";
 
 const Main = styled.div`
